56c13c7ba3ee72465b16d7e981a8da614e968676
Luizhenriqueup <luiz.up.work@gmail.com>
2026-05-18 21:37:38 -0300

    Update Ecommerce.java

--- E-commerce Atualizado 1205/Ecommerce.java
+++ E-commerce Atualizado 1205/Ecommerce.java
@@ -1,164 +1,256 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 
+import java.util.Scanner;
+
 class Cliente {
+
     private String nome;
 
-    public String getnome() {
+    public Cliente(String nome) {
+
+        this.nome = nome;
+
+    }
+
+    public String getNome() {
+
         return nome;
+
     }
 
-    public void setnome(String nome) {
-        this.nome = nome;
-    }
-
-    public Cliente(String nome){
-        this.nome = nome;
-    }
 }
- abstract class Produto {
+
+abstract class Produto {
+
     private String nome;
-    private double preço;
-    
-    public abstract double calcularPrecoFinal();
 
-    public Produto(String nome, double preço) {
+    protected double preco;
+
+    public Produto(String nome, double preco) {
+
         this.nome = nome;
-        this.preço = preço;
+
+        if (preco >= 0) {
+
+            this.preco = preco;
+
+        } else {
+
+            System.out.println("Preço Inválido!");
+
+        }
+
     }
 
-    public String getnome() {
+    abstract double calcularPrecoFinal();
+
+    public String getNome() {
+
         return nome;
+
     }
 
-    public Double getpreço() {
-        return preço;
+    public double getPreco() {
+
+        return preco;
+
     }
 
-    public void aplicarDesconto(double percentual) {
-        this.preço = this.preço - (this.preço * percentual / 100);
-    }
 }
- class ProdutoFisico extends Produto{
+
+class ProdutoFisico extends Produto {
+
     private double frete;
-    @Override
-    public double calcularPrecoFinal(){
-        return preço + frete;
+
+    public ProdutoFisico(String nome, double preco, double frete) {
+
+        super(nome, preco);
+
+        this.frete = frete;
+
     }
+
+    @Override
+
+    public double calcularPrecoFinal() {
+
+        return preco + frete;
+
+    }
+
 }
-class Produtodigital extends Produto{
-    @Override
-    public double calcularPrecoFinal(preço){
-        return preço;
+
+class ProdutoDigital extends Produto {
+
+    public ProdutoDigital(String nome, double preco) {
+
+        super(nome, preco);
+
     }
+
+    @Override
+
+    public double calcularPrecoFinal() {
+
+        return preco;
+
+    }
+
+}
+
+class ProdutoPromocional extends Produto {
+
+    private double desconto;
+
+    public ProdutoPromocional(String nome, double preco, double desconto) {
+
+        super(nome, preco);
+
+        this.desconto = desconto;
+
+    }
+
+    public double calcularPrecoFinal() {
+
+        return preco - (preco * desconto / 100);
+
+    }
+
 }
 
 class Carrinho {
+
     private ArrayList<Produto> produtos;
 
     public Carrinho() {
+
         this.produtos = new ArrayList<>();
+
     }
 
     public void adicionarProduto(Produto produto) {
-        if (produto.getpreço() > 0) {
-            produtos.add(produto);
-        } else {
-            System.out.println("Por favor adicionar um produto");
-        }
+
+        produtos.add(produto);
+
+        System.out.println(produto.getNome() + "Adicionado ao carrinho");
+
     }
 
     public void removerProduto(Produto produto) {
+
         produtos.remove(produto);
+
     }
 
     public double calcularTotal() {
+
         double total = 0;
+
         for (Produto p : produtos) {
+
             total += p.calcularPrecoFinal();
+
         }
+
         return total;
+
     }
 
     public void mostrarProdutos() {
+
         if (produtos.isEmpty()) {
-            System.out.println("O seu Carrinho está vazio");
+
+            System.out.println("Carrinho vazio.");
+
             return;
-        } else {
-            for (Produto p : produtos) {
-                System.out.println(p.getnome() + " R$ " + p.getpreço());
-            }
+
         }
+
+        for (Produto p : produtos) {
+
+            System.out.println(p.getNome() + " - R$" + p.calcularPrecoFinal());
+
+        }
+
     }
 
     public ArrayList<Produto> getProdutos() {
+
         return produtos;
+
     }
+
 }
 
 class Pedido {
+
     private Cliente cliente;
+
     private Carrinho carrinho;
 
     public Pedido(Cliente cliente, Carrinho carrinho) {
+
         this.cliente = cliente;
+
         this.carrinho = carrinho;
-    }
 
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public Carrinho getCarrinho() {
-        return carrinho;
     }
 
     public void mostrarResumo() {
-        System.out.println("=====RESUMO======");
-        System.out.println("Cliente: " + cliente.getnome());
-        System.out.println("Seus Produtos");
+
+        System.out.println("\n=======RESUMO DO PEDIDO=======");
+
+        System.out.println("CLiente" + cliente.getNome());
+
+        System.out.println("\nProdutos");
+
         carrinho.mostrarProdutos();
-        System.out.println("Total: R$" + carrinho.calcularTotal());
+
+        System.out.println("\nTotal: R$" + carrinho.calcularTotal());
 
     }
+
 }
 
-public class Ecommerce {
+class Main {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("DIGITE O SEU NOME");
-        String nomeCliente = entrada.nextLine();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do cliente: ");
+
+        String nomeCliente = scanner.nextLine();
+
         Cliente cliente = new Cliente(nomeCliente);
 
-        Produto p1 = new Produto("Celular", 2500);
-        Produto p2 = new Produto("Televisao", 4300);
-        Produto p3 = new Produto("Notebook", 3200);
+        Produto p1 = new ProdutoFisico("Notebook", 3000, 50);
 
-        p1.aplicarDesconto(20);
+        Produto p2 = new ProdutoDigital("Curso em JAVA", 300);
+
+        Produto p3 = new ProdutoPromocional("Mouse", 150, 10);
 
         Carrinho carrinho = new Carrinho();
 
-        boolean menu = true;
+        boolean continuar = true;
 
-        while (menu) {
-            System.out.println("\n===== MENU DE PRODUTOS =====");
+        while (continuar) {
 
-            System.out.println("1 - Celular - R$ " + p1.getpreço());
+            System.out.println("---MENU---");
 
-            System.out.println("2 - Televisao - R$ " + p2.getpreço());
+            System.out.println("1-Notebook");
 
-            System.out.println("3 - Notebook - R$ " + p3.getpreço());
+            System.out.println("2-Curso de JAVA");
 
-            System.out.println("4 - Remover produto do carrinho");
+            System.out.println("3-Mouse");
 
-            System.out.println("5 - Aplicar desconto em um produto");
+            System.out.println("4-Mostrar Carrinho");
 
-            System.out.println("0 - Finalizar compra");
+            System.out.println("0-Finalizar Compra");
 
-            System.out.print("Escolha uma opção: ");
+            System.out.println("Escolha uma opção: ");
 
-            int opcao = entrada.nextInt();
+            int opcao = scanner.nextInt();
 
             switch (opcao) {
 
@@ -166,15 +258,11 @@ public class Ecommerce {
 
                     carrinho.adicionarProduto(p1);
 
-                    System.out.println("Você adicionou Celular ao carrinho");
-
                     break;
 
                 case 2:
 
                     carrinho.adicionarProduto(p2);
-
-                    System.out.println("Você adicionou Televisao ao carrinho");
 
                     break;
 
@@ -182,101 +270,17 @@ public class Ecommerce {
 
                     carrinho.adicionarProduto(p3);
 
-                    System.out.println("Você adicionou Notebook ao carrinho");
-
                     break;
 
                 case 4:
 
-                    System.out.println("Produtos no carrinho: ");
-
                     carrinho.mostrarProdutos();
-
-                    System.out.print("Digite o nome do produto a remover: ");
-
-                    entrada.nextLine();
-
-                    String nomeRemover = entrada.nextLine();
-
-                    boolean removido = false;
-
-                    for (Produto p : carrinho.getProdutos()) {
-
-                        if (p.getnome().equalsIgnoreCase(nomeRemover)) {
-
-                            carrinho.removerProduto(p);
-
-                            System.out.println("Produto removido do carrinho");
-
-                            removido = true;
-
-                            break;
-
-                        }
-
-                    }
-
-                    if (!removido) {
-
-                        System.out.println("Produto não encontrado no carrinho.");
-
-                    }
-
-                    break;
-
-                case 5:
-
-                    System.out.println("Produtos disponíveis:");
-
-                    System.out.println("1 - Celular - R$ " + p1.getpreço());
-
-                    System.out.println("2 - Televisao - R$ " + p2.getpreço());
-
-                    System.out.println("3 - Notebook - R$ " + p3.getpreço());
-
-                    System.out.print("Digite o nome do produto para aplicar desconto: ");
-
-                    entrada.nextLine();
-
-                    String nomeDesconto = entrada.nextLine();
-
-                    Produto produtoDesconto = null;
-
-                    if (nomeDesconto.equalsIgnoreCase("Celular")) {
-
-                        produtoDesconto = p1;
-
-                    } else if (nomeDesconto.equalsIgnoreCase("Televisao")) {
-
-                        produtoDesconto = p2;
-
-                    } else if (nomeDesconto.equalsIgnoreCase("Notebook")) {
-
-                        produtoDesconto = p3;
-
-                    }
-
-                    if (produtoDesconto != null) {
-
-                        System.out.print("Digite o percentual de desconto: ");
-
-                        double percentual = entrada.nextDouble();
-
-                        produtoDesconto.aplicarDesconto(percentual);
-
-                        System.out.println("Desconto aplicado! Novo preço: R$ " + produtoDesconto.getpreço());
-
-                    } else {
-
-                        System.out.println("Produto não encontrado.");
-
-                    }
 
                     break;
 
                 case 0:
 
-                    menu = false;
+                    continuar = false;
 
                     System.out.println("Finalizando compra...");
 
@@ -284,18 +288,16 @@ public class Ecommerce {
 
                 default:
 
-                    System.out.println("Opção inválida!");
+                    System.out.println("Opção Inválido");
 
             }
 
         }
 
-        entrada.nextLine();
-
         Pedido pedido = new Pedido(cliente, carrinho);
 
         pedido.mostrarResumo();
 
-        entrada.close();
     }
+
 }
